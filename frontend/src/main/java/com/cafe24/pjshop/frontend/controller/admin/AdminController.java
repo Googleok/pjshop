457eb8a804ddc280ac352df359a145aa80ab71a3
Googleok <qkrwhddjr3@gmail.com>
2019-08-07 07:23:01 +0900

    front admin ui 추가

--- frontend/src/main/java/com/cafe24/pjshop/frontend/controller/admin/AdminController.java
+++ frontend/src/main/java/com/cafe24/pjshop/frontend/controller/admin/AdminController.java
@@ -1,8 +1,51 @@
 package com.cafe24.pjshop.frontend.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("adminController")
+@RequestMapping("/admin")
 public class AdminController {
 
+	@RequestMapping({"", "/main"})
+	public String adminMain() {
+		return "admin/index";
+	}
+	
+	@RequestMapping({"/login"})
+	public String adminLogin() {
+		return "admin/login";
+	}
+	
+	@RequestMapping({"/register"})
+	public String adminRegister() {
+		return "admin/register";
+	}
+	
+	@RequestMapping({"/forgot/password"})
+	public String adminForgotPassword() {
+		return "admin/forgot-password";
+	}
+	
+	@RequestMapping({"/error"})
+	public String adminError() {
+		return "admin/404";
+	}
+	
+	@RequestMapping({"/blank"})
+	public String adminBlank() {
+		return "admin/blank";
+	}
+	
+	@RequestMapping({"/charts"})
+	public String adminCharts() {
+		return "admin/charts";
+	}
+	
+	@RequestMapping({"/tables"})
+	public String adminTable() {
+		return "admin/tables";
+	}
+	
+	
 }
